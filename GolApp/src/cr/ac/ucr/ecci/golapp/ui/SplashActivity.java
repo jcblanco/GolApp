29b1aea0e597ca559afa8b20c0738fa34d3c6cbe
Juan Carlos Blanco C <juancablanco88@gmail.com>
2012-11-12 22:05:50 -0600

    Cree servicio para cargar tabla de posiciones

--- GolApp/src/cr/ac/ucr/ecci/golapp/ui/SplashActivity.java
+++ GolApp/src/cr/ac/ucr/ecci/golapp/ui/SplashActivity.java
@@ -4,11 +4,15 @@ import cr.ac.ucr.ecci.golapp.R;
 import cr.ac.ucr.ecci.golapp.R.layout;
 import cr.ac.ucr.ecci.golapp.R.menu;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class SplashActivity extends Activity {
 
+	Handler mHandler = new Handler();
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,4 +24,17 @@ public class SplashActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_splash, menu);
         return true;
     }
+    
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		mHandler.postDelayed(new Runnable() {
+
+			public void run() {
+				startActivity( new Intent(getApplicationContext(), TablaPosiciones.class));
+				finish();
+			}
+		}, 3000);
+	}
 }
--- GolApp/src/cr/ac/ucr/ecci/golapp/ui/TablaPosiciones.java
+++ GolApp/src/cr/ac/ucr/ecci/golapp/ui/TablaPosiciones.java
@@ -6,13 +6,18 @@ import cr.ac.ucr.ecci.golapp.R.menu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
 
 public class TablaPosiciones extends Activity {
 
+	ListView mTablaPosiciones;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabla_posiciones);
+        
+        mTablaPosiciones  = (ListView) findViewById(R.id.equipos_list);
     }
 
     @Override
