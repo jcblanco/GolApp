7af4e6d39881f0c437a5c08ea362ee121c9452f0
Juan Carlos Blanco C <juancablanco88@gmail.com>
2012-11-19 23:10:09 -0600

    Interfaz lista

--- GolApp/src/cr/ac/ucr/ecci/golapp/ui/SplashActivity.java
+++ GolApp/src/cr/ac/ucr/ecci/golapp/ui/SplashActivity.java
@@ -8,15 +8,47 @@ import android.os.Handler;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class SplashActivity extends Activity {
 
 	Handler mHandler = new Handler();
+	Animation rotar;
+	TextView appName;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        
+        appName = (TextView) findViewById(R.id.appName);
+        
+        rotar = AnimationUtils.loadAnimation(this, R.anim.rotation);
+        
+        rotar.setAnimationListener(new AnimationListener(){
+
+			public void onAnimationEnd(Animation arg0) {
+				appName.setVisibility(View.VISIBLE);
+				
+			}
+
+			public void onAnimationRepeat(Animation animation) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void onAnimationStart(Animation animation) {
+				appName.setVisibility(View.INVISIBLE);
+				
+			}
+        	
+        });
+        
+        appName.startAnimation(rotar);
     }
 
     @Override
